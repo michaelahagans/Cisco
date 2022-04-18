816f9701fba3b3e1058fa265dda348fad2552db2
michaelahagans <michael.a.hagans@gmail.com>
2022-04-18 10:41:20 -0500

    commit tests to add tests

---
+++ tests/test_operations.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_can_get_uptime():
+    pass
+
+
+def test_can_get_version():
+    pass
+
+
+def test_can_get_performance():
+    pass
+
+
+def test_can_get_exp_ports():
+    pass
